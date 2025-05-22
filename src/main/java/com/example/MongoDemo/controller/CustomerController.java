44dbc093648fe8a34f18d6aecd6c2c508e470e6c
Shreya Hegde <srhegde4@gmail.com>
2025-05-22 11:05:14 +0530

    CustomerController with sample URL

---
+++ src/main/java/com/example/MongoDemo/controller/CustomerController.java
@@ -0,0 +1,14 @@
+package com.example.MongoDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+    @GetMapping
+    public String testUrl(){
+        return "Customer feature works...";
+    }
+}
